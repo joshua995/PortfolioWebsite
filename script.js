926fc33dc885e0981f6582750527de90e879be38
joshua995 <114888100+joshua995@users.noreply.github.com>
2026-09-03 23:00:00 -0600

    updates

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ class MyFooter extends HTMLElement {
             <button onclick="setCurrentBackgroundTheme('none')">No Theme</button>
           </column>
         <row>
+        // <p>&copy 2026</p>
       </footer>
     `;
   }
