1bd128a8eec1a8f3f924bbb0a317ed75b7d030e5
joshua995 <114888100+joshua995@users.noreply.github.com>
2026-09-25 23:00:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ class MyFooter extends HTMLElement {
             <button onclick="setCurrentBackgroundTheme('none')">No Theme</button>
           </column>
         <row>
-        // <p>&copy 2026</p>
+        <p>&copy 2026</p>
       </footer>
     `;
   }
